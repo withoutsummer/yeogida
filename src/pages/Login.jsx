27435ab7bb0dbe9087eec9bfa0f96343259252bc
cinsy26 <siusy2618@naver.com>
2024-07-29 13:39:39 +0900

    초기 설정

---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Login() {
+
+    return (
+        <div>로그인 페이지</div> // 코드 작성 시 삭제해주세요!
+    )
+}
---
+++ src/pages/Sharetrip.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Sharetrip() {
+
+    return (
+        <div>여행 공유 페이지</div> // 코드 작성 시 삭제해주세요!
+    )
+}
