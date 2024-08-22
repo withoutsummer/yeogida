12657773649a2d2d498632b53890f7c0522cd66e
SEOYOUNG BAEK <baeksy6067@gmail.com>
2024-08-23 02:28:15 +0900

    마이페이지 친구목록&친구요청 slick으로 리스트 생성

--- src/pages/Friend.jsx
+++ src/pages/Friend.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import searchIcon from '../assets/icons/search_icon.png';
 import addIcon from '../assets/icons/friend_add_icon.png';
 import slashIcon from '../assets/icons/slash_icon.png';
+import deleteIcon from '../assets/icons/delete_friend_icon.png';
+import approveIcon from '../assets/icons/approve_request_icon.png';
+import rejectIcon from '../assets/icons/reject_request_icon.png';
 
 const HeaderStyle = styled.div `
     margin-top: 100px;
@@ -130,23 +133,61 @@ const SliderContainer = styled.div `
 
 const FriendListSlide = styled.div `
     width: 95% !important;
+    display: flex !important;
     height: 110px;
-    // 피그마랑 다름
-    box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.1);
-    border-radius: 15px;
-    border-left: 10px solid #508c4e;
     box-sizing: border-box;
+    align-items: center;
+
+    // 아래 요소들은 피그마랑 다름
+    box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    border-left: 10px solid #508c4e;
     margin: 10px 0 10px 15px;
 `;
 
-// ---------------친구 검색바 Component---------------
-function FriendSearchBar() {
-    const [inputValue, setInputValue] = useState('');
-    // const [isAddClicked, setIsAddClicked] = useState(0);
+const FriendImage = styled.div `
+    width: 90px;
+    height: 90px;
+    border-radius: 180px;
+    background-color: #e0e0e0;
+    margin: 0 30px 0 20px;
+`;
 
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
+const FriendName = styled.span `
+    font-size: 24px;
+    margin-bottom: 10px;
+`;
+
+const FriendId = styled.span `
+    font-size: 20px;
+    color: #707070;
+`;
+
+const ListIconStyle = styled.img `
+    height: 24px;
+    // display: ${(props) => (props.selected ? "block" : "none")};
+    cursor: pointer;
+    filter: invert(99%) sepia(0%) saturate(6%) hue-rotate(146deg) brightness(92%) contrast(92%);
+
+    &:hover {
+        filter: invert(95%) sepia(7%) saturate(4256%) hue-rotate(297deg) brightness(85%) contrast(132%);
     }
+`;
+
+const RequestIconStyle = styled.img `
+    height: 16px;
+    // display: ${(props) => (props.selected ? "block" : "none")};
+    cursor: pointer;
+    filter: invert(99%) sepia(0%) saturate(6%) hue-rotate(146deg) brightness(92%) contrast(92%);
+
+    &:hover {
+        filter: invert(95%) sepia(7%) saturate(4256%) hue-rotate(297deg) brightness(85%) contrast(132%);
+    }
+`;
+
+// ---------------친구 검색바 Component---------------
+function FriendSearchBar({ inputValue, handleChange }) {
+    // const [isAddClicked, setIsAddClicked] = useState(0);
 
     return (
         <SearchBarStyle>
@@ -174,62 +215,122 @@ function FriendSearchBar() {
 }
 
 // ---------------친구 목록 Component---------------
-function FriendList() {
-    const slideCount = 6;
+function ListAndRequest({ selected, inputValue }) {
+    // 슬라이더 ref
+    const sliderRef = useRef(null);
+
+    // 임시 데이터
+    const friendListData = [
+        { id: 1, name: 'mijin', friendId: 'kimmj5678' },
+        { id: 2, name: 'sieun', friendId: 'kose0987' },
+        { id: 3, name: 'seorin', friendId: 'chesr6543' },
+        { id: 4, name: 'seyeon', friendId: 'imsy2109' },
+        { id: 5, name: 'john', friendId: 'john1234' },
+        { id: 6, name: 'alice', friendId: 'alice5678' },
+        { id: 7, name: 'bob', friendId: 'bob2468' },
+        { id: 8, name: 'charlie', friendId: 'charlie8765' },
+        { id: 9, name: 'david', friendId: 'david0987' },
+        { id: 10, name: 'eve', friendId: 'eve5432' },
+    ];
+
+    const friendRequestData = [
+        { id: 1, name: 'hyeri', friendId: 'janghr8765' },
+        { id: 2, name: 'eunsu', friendId: 'koes4321' },
+        { id: 4, name: 'seyeon', friendId: 'imsy2109' },
+        { id: 5, name: 'john', friendId: 'john1234' },
+        { id: 6, name: 'alice', friendId: 'alice5678' },
+    ];
+
+    // isMiniMenuClicked 값에 따라 로딩할 데이터를 선택
+    const dataToShow = selected ? friendListData : friendRequestData;
+
+    // inputValue로 친구 목록을 필터링
+    const filteredData = dataToShow.filter(friend => 
+        friend.friendId.includes(inputValue)
+    );
 
     const settings = {
         dots: true,
-        infinite: slideCount > 4,
+        infinite: filteredData.length > 4,  // 현재 선택된 데이터의 길이에 따라 설정
         speed: 500,
-        slidesToShow: 1,  // 한 번에 1개의 슬라이드만 보여줌
+        slidesToShow: 1,  
         slidesToScroll: 1,
         rows: 4,
-        slidesPerRow: 1
+        slidesPerRow: 1,
     };
 
+    useEffect(() => {
+        if (sliderRef.current) {
+            sliderRef.current.slickGoTo(0);
+        }
+    }, [selected, inputValue, filteredData]);
+    
+
     return (
-        <>
-            <SliderContainer>
-                <StyledSlider {...settings}>
-                    <FriendListSlide>
-                        
-                    </FriendListSlide>
-                    <FriendListSlide>
-                        
-                    </FriendListSlide>
-                    <FriendListSlide>
-                        
-                    </FriendListSlide>
-                    <FriendListSlide>
-                        
-                    </FriendListSlide>
-                    <FriendListSlide>
-                        
-                    </FriendListSlide>
-                    <FriendListSlide>
-                        
-                    </FriendListSlide>
-                </StyledSlider>
-            </SliderContainer>
-        </>
+        <SliderContainer>
+            <StyledSlider ref={sliderRef} {...settings} key={filteredData.length}>
+            {filteredData.map((friend) => (
+                <FriendListSlide key={friend.id}>
+                    <FriendImage />
+                    <div 
+                        style={{ 
+                            display: 'flex',
+                            flexDirection: 'column'
+                    }}>
+                        <FriendName>{friend.name}</FriendName>
+                        <FriendId>{friend.friendId}</FriendId>
+                    </div>
+                    {/* 친구 삭제 아이콘 */}
+                    {selected && (
+                        <ListIconStyle 
+                            src={deleteIcon} 
+                            alt='친구 삭제 아이콘' 
+                            style={{ margin: '0 20px 0 auto' }}
+                            onClick={() => console.log(`${friend.name} 삭제 버튼 click`)}
+                        />
+                    )}
+                    {/* 친구 요청 거절 아이콘 */}
+                    {!selected && (
+                        <RequestIconStyle 
+                            src={rejectIcon} 
+                            alt='친구요청 거절 아이콘' 
+                            style={{ marginLeft: 'auto' }}
+                            onClick={() => console.log(`${friend.name} 요청 거절 버튼 click`)}
+                        />
+                    )}
+                    {/* 친구 요청 승인 아이콘 */}
+                    {!selected && (
+                        <RequestIconStyle 
+                            src={approveIcon} 
+                            alt='친구요청 승인 아이콘' 
+                            style={{ margin: '0 20px 0 30px' }}
+                            onClick={() => console.log(`${friend.name} 요청 승인 버튼 click`)}
+                        />
+                    )}
+                </FriendListSlide>
+            ))}
+            </StyledSlider>
+        </SliderContainer>
     )
 }
 
-// ---------------친구 요청 Component---------------
-function FriendRequest() {
-    return (
-        <div>
-            친구 요청
-        </div>
-    )
-}
+
 
 export default function Friend() {
     const [isMiniMenuClicked, setIsMiniMenuClicked] = useState(0);
+    const [inputValue, setInputValue] = useState('');
 
     const handleClick = (index) => {
         setIsMiniMenuClicked(index);
     }
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    // isMiniMenuClicked 변경 시 inputValue 초기화
+    useEffect(() => {
+        setInputValue('');
+    }, [isMiniMenuClicked]);
 
     return (
         <div>
@@ -242,7 +343,10 @@ export default function Friend() {
                 {/* 친구 목록 / 친구 요청 */}
                 <article>
                     {/* 친구 검색바 */}
-                    <FriendSearchBar />
+                    <FriendSearchBar 
+                        inputValue={inputValue}
+                        handleChange={handleChange}
+                    />
                     
                     <ArticleStyle>
                         {/* 버튼 & 드롭다운 */}
@@ -269,11 +373,10 @@ export default function Friend() {
                             </div>
                         </MiniNavStyle>
                         {/* 목록 */}
-                        {isMiniMenuClicked ? (
-                            <FriendRequest />
-                        ) : (
-                            <FriendList />
-                        )}
+                        <ListAndRequest 
+                            selected={ isMiniMenuClicked === 0 }
+                            inputValue={inputValue}
+                        />
                     </ArticleStyle>
                 </article>
             </section>
